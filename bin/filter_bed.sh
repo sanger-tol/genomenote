e30a4efb671d86db4757748239db4bd03328dee3
Priyanka Surana <ps22@sanger.ac.uk>
2023-05-03 19:08:51 +0100

    Update bin/filter_bed.sh
    Co-authored-by: Matthieu Muffato <mm49@sanger.ac.uk>

--- bin/filter_bed.sh
+++ bin/filter_bed.sh
@@ -3,4 +3,9 @@
 infile=$1
 outfile=$2
 
-cat $infile | paste -d '\t' - - | sed 's/-/_/g' | awk 'BEGIN {FS="\t"; OFS="\t"} { if ($1 > $7) { print substr($4,1,length($4)-2),$12,$7,$8,"16",$6,$1,$2,"8",$11,$5 } else { print substr($4,1,length($4)-2),$6,$1,$2,"8",$12,$7,$8,"16",$5,$11} }' | tr '_+' '01' | awk 'NF==11' > $outfile
+cat $infile |
+    paste -d '\t' - - |
+    sed 's/-/_/g' |
+    awk 'BEGIN {FS="\t"; OFS="\t"} { if ($1 > $7) { print substr($4,1,length($4)-2),$12,$7,$8,"16",$6,$1,$2,"8",$11,$5 } else { print substr($4,1,length($4)-2),$6,$1,$2,"8",$12,$7,$8,"16",$5,$11} }' |
+    tr '_+' '01' |
+    awk 'NF==11' > $outfile
