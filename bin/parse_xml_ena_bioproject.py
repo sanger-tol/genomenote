f6c97a82a9a69dcc740225630842bd3e50f704c1
Bethan Yates <by3@sanger.ac.uk>
2023-06-07 16:50:40 +0100

    Fixed black linting

--- bin/parse_xml_ena_bioproject.py
+++ bin/parse_xml_ena_bioproject.py
@@ -6,8 +6,8 @@ import argparse
 import xml.etree.ElementTree as ET
 
 fetch = [
-  ("BIOPROJECT_ACCESSION", ["PROJECT"], ("attrib", "accession")),
-  ("BIOPROJECT_TITLE", ["PROJECT", "TITLE"]),
+    ("BIOPROJECT_ACCESSION", ["PROJECT"], ("attrib", "accession")),
+    ("BIOPROJECT_TITLE", ["PROJECT", "TITLE"]),
 ]
 
 def parse_args(args=None):
@@ -17,81 +17,81 @@ def parse_args(args=None):
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
     parser.add_argument("FILE_IN", help="Input XML Bioproject file.")
     parser.add_argument("FILE_OUT", help="Output file.")
-    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+    parser.add_argument("--version", action="version", version="%(prog)s 1.0")
     return parser.parse_args(args)
 
 
 def make_dir(path):
-  if len(path) > 0:
-    os.makedirs(path, exist_ok=True)
+    if len(path) > 0:
+        os.makedirs(path, exist_ok=True)
 
 
 def print_error(error, context="Line", context_str=""):
     error_str = "ERROR: Please check xml file -> {}".format(error)
     if context != "":
-      if context_str != "":
-        error_str = "ERROR: Please check xml file -> {}\n{}: '{}'".format(
-            error, context.strip(), context_str.strip()
-        )
-      else: 
-        error_str = "ERROR: Please check xml file -> {}\n{}".format(
-            error, context.strip()
-        )
+        if context_str != "":
+            error_str = "ERROR: Please check xml file -> {}\n{}: '{}'".format(
+                error, context.strip(), context_str.strip()
+          )
+        else: 
+            error_str = "ERROR: Please check xml file -> {}\n{}".format(
+                error, context.strip()
+            )
 
     print(error_str)
     sys.exit(1)    
 
 def parse_xml(file_in, file_out):
 
-  tree = ET.parse(file_in)
-  root = tree.getroot()
-  param_list = []
+    tree = ET.parse(file_in)
+    root = tree.getroot()
+    param_list = []
 
-  for f in fetch:
-    r = root
-    max_depth = len(f[1])
-    fn = len(f)
-    i = 0
+    for f in fetch:
+        r = root
+        max_depth = len(f[1])
+        fn = len(f)
+        i = 0
 
-    for tag in f[1]: 
-      i+=1
+        for tag in f[1]: 
+            i+=1
 
-      r =  r.find(tag)
-      ## Handle cases where parameter is not available for this PROJECT
-      if r is None: 
-        break
+            r =  r.find(tag) 
+            ## Handle cases where parameter is not available for this PROJECT
+            if r is None: 
+                break
 
-      if (i == max_depth):
-        ## Handle more complex cases where not just fetching text for an element 
-        if fn == 3:
+            if (i == max_depth):
+                ## Handle more complex cases where not just fetching text for an element 
+                if fn == 3:
 
-          ## Fetch specific attribute for a given element 
-          if f[2][0] == "attrib":
-            try:
-              param = r.attrib.get(f[2][1])
-            except ValueError:
-              param = None
+                    ## Fetch specific attribute for a given element 
+                    if f[2][0] == "attrib":
+                        try:
+                            param = r.attrib.get(f[2][1])
+                        except ValueError:
+                            param = None
 
-        else: 
-          try:
-            param = r.text
-          except ValueError:
-            param = None
+                else: 
+                    try:
+                        param = r.text
+                    except ValueError:
+                        param = None
 
         if param is not None:
-          param_list.append([f[0], param])
+            param_list.append([f[0], param])
 
 
-  if len(param_list) > 0:
-    out_dir = os.path.dirname(file_out)
-    make_dir(out_dir)
-    with open(file_out, "w") as fout:
-      fout.write(",".join(["#paramName", "paramValue"]) + "\n")
-      for param_pair in param_list:
-        fout.write(",".join(param_pair) + "\n")
+    if len(param_list) > 0:
+        out_dir = os.path.dirname(file_out)
+        make_dir(out_dir)
+        with open(file_out, "w") as fout:
+            fout.write(",".join(["#paramName", "paramValue"]) + "\n")
+            for param_pair in param_list:
+              fout.write(",".join(param_pair) + "\n")
 
-  else:
-    print_error("No parameters found!", "File: {}".format(file_in))
+    else:
+        print_error("No parameters found!", "File: {}".format(file_in))
 
 def main(args=None):
     args=parse_args(args)
